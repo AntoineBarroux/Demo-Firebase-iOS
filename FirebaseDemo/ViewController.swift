ce7f2c88b5e5df2d2596f52f214f2ea461df9c06
AntoineBarroux <antoine.barroux@gmail.com>
2017-01-23 13:19:13 +0100

    Adding Cobalt/HTTP Request

--- FirebaseDemo/ViewController.swift
+++ FirebaseDemo/ViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseMessaging
 
-class ViewController: UIViewController {
+class ViewController: CobaltViewController, CobaltDelegate {
+    
+    //MARK : LIFECYCLE
 
+    required init?(coder: NSCoder){
+        super.init(coder: coder)
+        self.setDelegate(self)
+        Cobalt.setResourcePath(Bundle.main.resourcePath! + "/common/")
+        initWithPage("index.html", andController: "default")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +33,30 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    //MARK : DELEGATE
+    
+    func onUnhandledEvent(_ event: String, withData data: [AnyHashable : Any], andCallback callback: String) -> Bool{
+        if (event == "getToken"){
+            print("catch de l'event")
+            let data = ["token":  FIRInstanceID.instanceID().token()!]
+            let nsdata:NSDictionary = data as NSDictionary
+            
+            sendEvent("envoiToken", withData: nsdata, andCallback: nil)
+            return true
+        }
+        return false
+    }
+    
+    func onUnhandledMessage(_ message: [AnyHashable : Any]) -> Bool{
+        return false
+    }
+    
+    func onUnhandledCallback(_ callback: String, withData data: [AnyHashable : Any]) -> Bool{
+        return false
     }
 
 
